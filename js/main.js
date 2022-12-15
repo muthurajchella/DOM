ce8f96929cb39e0ca2ef170ffbc294d077640439
Muthurajchella- <muthuraj.chella@gmail.com>
2022-12-15 17:02:14 +0530

    Dom

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//application programing interface
+
+function f(){
+document.getElementById("h1").innerHTML="Hello World!";
+}
+
+let obj1={
+    name:"Muthu",
+};
+
+function a(){
+    var t=document.getElementById("p1");
+    t.style.color="red";
+    t.style.backgroundColor="yellow";
+    t.style.fontSize="50px";
+}
+
+
+    let t=2+3;
+    document.getElementById("p2").innerHTML=t;
